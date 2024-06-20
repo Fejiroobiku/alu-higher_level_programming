ff808d22ed501ff0009eecd3c74d321ba77c6d5a
Fejirooobiku <o.obiku@alustudent.com>
2024-06-20 03:50:31 -0700

    Bro

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-class CustomClass:
-    def __repr__(self):
-        return 'object-oriented programming with Python'
-
-print(CustomClass())    
+str = "Python is an interpreted, interactive, object-oriented programming\
+  language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
