5e80c463390c37f196cba5cc2afdbb888b5657dd
Fejirooobiku <o.obiku@alustudent.com>
2024-06-21 07:06:57 -0700

    J

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,12 @@
-#!/usr/bin/python3
-from sys import argv
+#!/usr/bin/env python3
+import sys
 
 if __name__ == "__main__":
-    args = argv[1:]
-    result = sum(int(arg) for arg in args)
-    print(result)
+    # Get all command-line arguments except the first one (script name)
+    args = sys.argv[1:]
+    
+    # Convert arguments to integers and sum them
+    total = sum(int(arg) for arg in args)
+    
+    # Print the result
+    print(total)
