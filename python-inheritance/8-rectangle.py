2606c56c9c88524d56ad7e7325d631cede2c8069
Fejirooobiku <o.obiku@alustudent.com>
2024-07-03 13:16:03 -0700

    H

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,50 +1,13 @@
 #!/usr/bin/python3
-"""
-Module defining Rectangle class inheriting from BaseGeometry.
-
-Contains Rectangle class that represents a rectangle with width and height.
-"""
-
+'''Module for Rectangle class.'''
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """
-    Represents a rectangle.
-
-    Attributes:
-        __width (int): Width of the rectangle.
-        __height (int): Height of the rectangle.
-    """
-
+    '''A subclass representing a rectangle.'''
     def __init__(self, width, height):
-        """
-        Initializes a Rectangle instance.
-
-        Args:
-            width (int): Width of the rectangle.
-            height (int): Height of the rectangle.
-        """
+        '''Constructor.'''
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-    def area(self):
-        """
-        Calculates the area of the rectangle.
-
-        Returns:
-            int: Area of the rectangle.
-        """
-        return self.__width * self.__height
-
-    def __str__(self):
-        """
-        Returns a string representation of the rectangle.
-
-        Returns:
-            str: String representation of the rectangle in the format "[Rectangle] width/height".
-        """
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
