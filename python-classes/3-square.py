ed92e9a2dedd217a844d9b7b921010a0b2a11800
Fejirooobiku <o.obiku@alustudent.com>
2024-07-03 10:01:23 -0700

    H

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -9,3 +9,50 @@ class Square:
 
     def area(self):
         return self.__size ** 2
+    class Square:
+    """
+    A class used to represent a Square.
+
+    ...
+
+    Attributes
+    ----------
+    __size : int
+        The size of the square (private attribute)
+
+    Methods
+    -------
+    area():
+        Returns the area of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Parameters
+        ----------
+        size : int, optional
+            The size of the square (default is 0)
+
+        Raises
+        ------
+        TypeError
+            If size is not an integer.
+        ValueError
+            If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates and returns the area of the square.
+
+        Returns
+        -------
+        int
+            The area of the square.
+        """
+        return self.__size ** 2
