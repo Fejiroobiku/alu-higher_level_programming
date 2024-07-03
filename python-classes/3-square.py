15caaf9cbb7a6a5cf722dcd13a9f9ffc57977ecd
Fejirooobiku <o.obiku@alustudent.com>
2024-07-03 13:40:19 -0700

    H

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,58 +1,21 @@
 #!/usr/bin/python3
+"""Defines a class square"""
+
+
 class Square:
+    """Represent a square."""
+
     def __init__(self, size=0):
+        """Initialize a new square.
+        Args:
+        size (int): The size of the new square
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
-        return self.__size ** 2
-    class Square:
-    """
-    A class used to represent a Square.
-
-    ...
-
-    Attributes
-    ----------
-    __size : int
-        The size of the square (private attribute)
-
-    Methods
-    -------
-    area():
-        Returns the area of the square.
-    """
-
-    def __init__(self, size=0):
-        """
-        Parameters
-        ----------
-        size : int, optional
-            The size of the square (default is 0)
-
-        Raises
-        ------
-        TypeError
-            If size is not an integer.
-        ValueError
-            If size is less than 0.
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
-
-    def area(self):
-        """
-        Calculates and returns the area of the square.
-
-        Returns
-        -------
-        int
-            The area of the square.
-        """
-        return self.__size ** 2
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
